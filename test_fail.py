ea4aaa0aadffba09581465ad348e32a6f76d64de
PuzankoNikolay <igorzenmat@gmail.com>
2024-03-06 20:00:05 +0300

    Ex10-13

---
+++ test_fail.py
@@ -0,0 +1,22 @@
+import requests
+import pytest
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestFail(BaseCase):
+    def setup(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+    def test_auth_user(self):
+        response2 = requests.get(
+            "https://playground.learnqa.ru/api/user/auth",
+            headers={"x-csrf-token":self.token},
+            cookies={"auth_sid":self.auth_sid}
+        )
